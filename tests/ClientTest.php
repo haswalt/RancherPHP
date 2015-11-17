c18569f07428cd26c430b82194af44ffbe421682
Harry Walter <harry.walter@lqdinternet.com>
2015-11-17 23:08:58 +0000

    Use real rancher instance for tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -8,7 +8,7 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetClient()
     {
-        $client = new Client(null, null, null);
+        $client = new Client('http://127.0.0.1:8080/', null, null);
         $this->assertInstanceOf("\\GuzzleHttp\\Client", $client->getClient());
     }
 }
