8c23f876dfadc56b2a8cf0f18110791063eef902
Harry Walter <harry.walter@lqdinternet.com>
2015-11-17 23:14:58 +0000

    Clearup before writing real tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -8,7 +8,6 @@ class ClientTest extends \PHPUnit_Framework_TestCase
 {
     public function testGetClient()
     {
-        $client = new Client('http://127.0.0.1:8080/', null, null);
-        $this->assertInstanceOf("\\GuzzleHttp\\Client", $client->getClient());
+        $this->markTestIncomplete('Test incomplete');
     }
 }
